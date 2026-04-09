4fe31c684a7d3d5789ac0428e998fa161a060f72
pojymomdoh-design <pojymomdoh@gmail.com>
2026-04-09 22:38:38 +0300

    Update tokens.js

--- tokens.js
+++ tokens.js
@@ -1,8 +1,8 @@
 // status can be "online", "idle", "dnd", or "invisible" or "offline"
 export default [
     {
-        channelId: "1138774964254822420",
-        serverId: "1448817318246355075",
+        channelId: "1448817318246355075",
+        serverId: "1138774964254822420",
         token: process.env.token1,
         selfDeaf: false,
         autoReconnect: {
